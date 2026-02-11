ac52d330bfba3ec1888f4e691b19a14e34c74d3d
mahsa <mahsa.jani2002@gmail.com>
2026-02-12 01:06:52 +0330

    Hero.h edited

--- include/hero.h
+++ include/hero.h
@@ -1,26 +1,26 @@
 #ifndef HERO_H
 #define HERO_H
 
-typedef struct {
-    int life_points;  
-    int coins;                 
-    int sword;                    
-    int armor;      
-    int health_potions;            
-
-    int first_mission_completed;  
-    int second_mission_completed; 
-    int third_mission_completed; 
-    int final_mission_unlocked;   
+typedef struct
+{
+    int hp;
+    int money;
+    int sword;
+    int armor;
+    int potions;
+    int m1;
+    int m2;
+    int m3;
+    int final;
 } Hero;
 
-void Hero_coins_change(Hero* h, int coins);
-void Hero_life_points_change(Hero* h, int life_points);
-void Hero_equip_sword(Hero* h);
-void Hero_equip_hero_sword(Hero* h);
-void Hero_equip_armor(Hero* h);
-void Hero_complete_mission(Hero* h, int mission_number);
-void Hero_add_health_potion(Hero* h);
-void Hero_use_health_potion(Hero* h);
+void Hero_coins_change(Hero *h, int coins);
+void Hero_life_points_change(Hero *h, int life_points);
+void Hero_equip_sword(Hero *h);
+void Hero_equip_hero_sword(Hero *h);
+void Hero_equip_armor(Hero *h);
+void Hero_complete_mission(Hero *h, int mission_number);
+void Hero_add_health_potion(Hero *h);
+void Hero_use_health_potion(Hero *h);
 
-#endif 
+#endif
