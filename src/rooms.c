cc11859e8af6f934e02b74815b9636eb17803c19
mahsa <mahsa.jani2002@gmail.com>
2026-02-13 18:07:33 +0330

    Fix mysterious chest flip coin mechanic and clean up includes

--- src/rooms.c
+++ src/rooms.c
@@ -1,15 +1,15 @@
 #include <rooms.h>
-#include <dice.h>
-#include <hero.h>
-#include <sword.h>
+#include "../include/hero.h"
+#include "../include/sword.h"
+#include "../include/dice.h"
 #include <string.h>
+#include <stdlib.h>
 
-int DUNGEON_TO_ROOM[3][6] = {{WILD_DOG,GOBLIN,SKELETON,ORC,POISONOUS_BOG,ORC_GENERAL},
-{DARK_HATCH,BAT,ZOMBIE,PHANTOM,GREAT_VAMPHIRE,GURDIAN_DEMON},
-{EMPTY_ROOM,FALLING_CRYSTALS,UNSAFE_BRIDGE,MYSTERIOUS_CHEST,STEEP_CLIFF,ANCIENT_DRAGON}};
+int DUNGEON_TO_ROOM[3][6] = {{WILD_DOG, GOBLIN, SKELETON, ORC, POISONOUS_BOG, ORC_GENERAL},
+                             {DARK_HATCH, BAT, ZOMBIE, PHANTOM, GREAT_VAMPHIRE, GURDIAN_DEMON},
+                             {EMPTY_ROOM, FALLING_CRYSTALS, UNSAFE_BRIDGE, MYSTERIOUS_CHEST, STEEP_CLIFF, ANCIENT_DRAGON}};
 
-
-char* DUNGEON_TO_NAME[18]={
+char *DUNGEON_TO_NAME[18] = {
     "Empty Room",
     "Wild Dog",
     "Goblin",
@@ -27,15 +27,12 @@ char* DUNGEON_TO_NAME[18]={
     "Unsafe Bridge",
     "Mysterious Chest",
     "Steep Cliff",
-    "Ancient Dragon"
-};
+    "Ancient Dragon"};
 
-
-
-Room generate_room(RoomName room_name,Hero* h)
+Room generate_room(RoomName room_name, Hero *h)
 {
     Room room;
-    room.name=room_name;
+    room.name = room_name;
     if (room_name == WILD_DOG)
     {
         room.type = ENEMY;
@@ -60,109 +57,119 @@ Room generate_room(RoomName room_name,Hero* h)
     else if (room_name == ORC)
     {
         room.type = ENEMY;
-        room.fatal_strike = 3; 
+        room.fatal_strike = 3;
         room.damage = 4;
         room.reward_coins = 6;
     }
     else if (room_name == POISONOUS_BOG)
     {
         room.type = TRAP;
-        room.fatal_strike = 0; 
+        room.fatal_strike = 0;
         room.damage = roll_dice();
         room.reward_coins = 0;
     }
     else if (room_name == ORC_GENERAL)
     {
         room.type = ENEMY;
-        if (h->sword==HERO_SWORD)
+        if (h->sword == HERO_SWORD)
         {
-            room.fatal_strike = 5; 
+            room.fatal_strike = 5;
         }
         else
-        room.fatal_strike = 6; 
+            room.fatal_strike = 6;
         room.damage = 3;
         room.reward_coins = 12;
     }
-    else if (room_name==DARK_HATCH)
+    else if (room_name == DARK_HATCH)
     {
         room.type = TRAP;
-        room.fatal_strike = 0; 
+        room.fatal_strike = 0;
         room.damage = 3;
         room.reward_coins = 0;
     }
-    else if (room_name==BAT)
+    else if (room_name == BAT)
     {
         room.type = ENEMY;
-        room.fatal_strike = 2; 
+        room.fatal_strike = 2;
         room.damage = 2;
         room.reward_coins = 1;
     }
-    else if (room_name==ZOMBIE)
+    else if (room_name == ZOMBIE)
     {
         room.type = ENEMY;
-        room.fatal_strike = 3; 
+        room.fatal_strike = 3;
         room.damage = 2;
         room.reward_coins = 2;
     }
-    else if (room_name==PHANTOM)
+    else if (room_name == PHANTOM)
     {
         room.type = ENEMY;
-        room.fatal_strike = 5; 
+        room.fatal_strike = 5;
         room.damage = 2;
         room.reward_coins = 4;
     }
-    else if (room_name==GREAT_VAMPHIRE)
+    else if (room_name == GREAT_VAMPHIRE)
     {
         room.type = ENEMY;
-            room.fatal_strike = 4; 
-            room.damage = 4;
-            room.reward_coins = 7;
+        room.fatal_strike = 4;
+        room.damage = 4;
+        room.reward_coins = 7;
     }
-    else if (room_name==GURDIAN_DEMON)
+    else if (room_name == GURDIAN_DEMON)
     {
         room.type = ENEMY;
-        room.fatal_strike = 4; 
+        room.fatal_strike = 4;
         room.damage = 6;
         room.reward_coins = 10;
     }
-    else if (room_name==FALLING_CRYSTALS)
+    else if (room_name == FALLING_CRYSTALS)
     {
         room.type = TRAP;
-        room.fatal_strike = 0; 
+        room.fatal_strike = 0;
         room.damage = 2;
         room.reward_coins = 0;
     }
-    else if (room_name==UNSAFE_BRIDGE)
+    else if (room_name == UNSAFE_BRIDGE)
     {
         room.type = TRAP;
-        room.fatal_strike = 0; 
+        room.fatal_strike = 0;
         room.damage = 0;
         room.reward_coins = -3;
     }
-    else if (room_name==MYSTERIOUS_CHEST)
+    else if (room_name == MYSTERIOUS_CHEST)
     {
         room.type = TRAP;
-        room.fatal_strike = 0; 
-        room.damage = 0;//TODO
-        room.reward_coins = 0;//TODO
+        room.fatal_strike = 0;
+
+        int coin = rand() % 2;
+        if (coin == 0)
+        {
+            room.damage = 0;
+            room.reward_coins = 10;
+        }
+        else
+        {
+            room.damage = 2;
+            room.reward_coins = 0;
+        }
     }
-    else if (room_name==STEEP_CLIFF)
+    else if (room_name == STEEP_CLIFF)
     {
         room.type = TRAP;
-        room.fatal_strike = 0; 
+        room.fatal_strike = 0;
         room.damage = roll_dice();
         room.reward_coins = 0;
     }
-    else if (room_name==ANCIENT_DRAGON)
+    else if (room_name == ANCIENT_DRAGON)
     {
         room.type = ENEMY;
-            room.fatal_strike = 5; 
-            room.damage = 10;
-            room.reward_coins = 12;
+        room.fatal_strike = 5;
+        room.damage = 10;
+        room.reward_coins = 12;
     }
     else
     {
-        room.type = EMPTY; 
+        room.type = EMPTY;
         room.fatal_strike = 0;
         room.damage = 0;
         room.reward_coins = 0;
